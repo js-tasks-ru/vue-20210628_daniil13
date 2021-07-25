93f4d23b86438cdc7097f76dcb7589cc2a8f2463
dalegray <levshindaniil93s@mail.ru>
2021-07-25 22:17:47 +0300

    Counter Button App

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,14 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const RootComponent = defineComponent({
+    data() {
+        return {
+            index: 0,
+        }
+    },
+})
+
+const app = createApp(RootComponent);
+const vm = app.mount('#app');
