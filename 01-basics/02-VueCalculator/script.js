17060fcd48b1fcb5785d2ae94e0bcaf34ba9f6f0
dalegray <levshindaniil93s@mail.ru>
2021-07-28 22:17:25 +0300

    Vue Calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,32 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const RootComponent = defineComponent({
+  data() {
+    return {
+      value1: 0,
+      value2: 0,
+      result: 0,
+      radio: 'sum',
+    };
+  },
+
+  computed: {
+    calcResult() {
+      switch(this.radio) {
+        case 'sum': 
+          return (this.result = Number(this.value1) + Number(this.value2))
+        case 'subtract': 
+          return (this.result = this.value1 - this.value2)
+        case 'multiply': 
+          return (this.result = this.value1 * this.value2)
+        case 'divide': 
+          return (this.result = this.value1 / this.value2)
+        default: 
+          return (this.result = Number(this.value1) + Number(this.value2))
+      }
+    },
+  },
+})
+
+const app = createApp(RootComponent);
+const vm = app.mount('#app');
