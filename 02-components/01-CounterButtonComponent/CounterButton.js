c87477536da37924de45caa7e2711775dee839d5
dalegray <levshindaniil93s@mail.ru>
2021-08-15 17:43:54 +0300

    5 tasks

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,22 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: () => 0,
+      required: true,
+    },
+  },
+  emits: ['update:count'],
 
+  methods: {
+    buttonClick(value) {
+      this.$emit('update:count', value + 1);
+    },
+  },
   // Шаблон лучше держать максимально простым, а логику выносить в методы
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click="buttonClick(count)">{{ count }}</button>`,
 });
